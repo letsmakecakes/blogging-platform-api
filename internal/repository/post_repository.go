422ea30ce321be6def2a4b467858a4599365b57d
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-16 20:50:23 +0530

    added method get a blog post by id from the database

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -29,3 +29,18 @@ func (r *blogRepository) Create(blog *models.Blog) error {
 	err := r.db.QueryRow(query, blog.Title, blog.Content, blog.Category, tags).Scan(&blog.ID, &blog.CreatedAt, &blog.UpdatedAt)
 	return err
 }
+
+func (r *blogRepository) GetByID(id int) (*models.Blog, error) {
+	query := `SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE id = $1`
+	row := r.db.QueryRow(query, id)
+
+	var blog models.Blog
+	var tags string
+	err := row.Scan(&blog.ID, &blog.Title, &blog.Category, &tags, &blog.CreatedAt, &blog.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	blog.Tags = strings.Split(tags, ",")
+	return &blog, nil
+}
