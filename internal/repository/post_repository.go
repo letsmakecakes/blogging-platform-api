b9fd7e990535d1f6ec66a5cb76b595c0ffe2da09
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-16 20:02:13 +0530

    added method to return blog repository object

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -10,9 +10,13 @@ type BlogRepository interface {
 	GetByID(id int) (*models.Blog, error)
 	GetAll(term string) ([]*models.Blog, error)
 	Update(post *models.Blog) error
-	Delete(id int) error 
+	Delete(id int) error
 }
 
 type blogRepository struct {
 	db *sql.DB
+}
+
+func NewPostRepository(db *sql.DB) BlogRepository {
+	return &blogRepository{db}
 }
