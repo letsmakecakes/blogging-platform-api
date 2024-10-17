ed48bd3ff5caae74b9adf1a0a038abcab33af97c
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-17 20:28:09 +0530

    added method to validate post

---
+++ internal/utils/validation.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"bloggingplatformapi/internal/models"
+	"errors"
+	"strings"
+)
+
+func ValidatePost(blog *models.Blog) error {
+	if strings.TrimSpace(blog.Title) == "" {
+		return errors.New("Title is required")
+	}
+	if strings.TrimSpace(blog.Content) == "" {
+		return errors.New("Content is required")
+	}
+	if strings.TrimSpace(blog.Category) == "" {
+		return errors.New("Category is required")
+	}
+	if len(blog.Tags) == 0 {
+		return errors.New("At least one tag is required")
+	}
+	return nil
+}
