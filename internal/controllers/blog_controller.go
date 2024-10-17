91009519d67228c8104e5eca886d8f93b3da564e
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-17 20:31:33 +0530

    added method to create a blog post

--- internal/controllers/blog_controller.go
+++ internal/controllers/blog_controller.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "bloggingplatformapi/internal/services"
+import (
+	"bloggingplatformapi/internal/models"
+	"bloggingplatformapi/internal/services"
+	"bloggingplatformapi/internal/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type BlogController struct {
 	Service services.BlogService
@@ -8,4 +15,26 @@ type BlogController struct {
 
 func NewBlogController(service services.BlogService) *BlogController {
 	return &BlogController{service}
+}
+
+// CreatePost handles POST /posts
+func (c *BlogController) CreateBlog(ctx *gin.Context) {
+	var blog models.Blog
+	if err := ctx.ShouldBindJSON(&blog); err != nil {
+		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	// Validate the post
+	if err := utils.ValidateBlog(&blog); err != nil {
+		utils.RespondWithError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := c.Service.CreateBlog(&blog); err != nil {
+		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to create blog")
+		return
+	}
+
+	utils.RespondWithJSON(ctx, http.StatusCreated, blog)
 }
