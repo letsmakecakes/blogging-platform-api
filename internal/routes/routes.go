d544355e0aca7e6e7f8180d5a4e36041e2c74d5f
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-17 21:11:10 +0530

    added method for routing

---
+++ internal/routes/routes.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"bloggingplatformapi/internal/controllers"
+	"bloggingplatformapi/internal/repository"
+	"bloggingplatformapi/internal/services"
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(router *gin.Engine, db *sql.DB) {
+	// Initialize repository, service, and controller
+	blogRepo := repository.NewBlogRepository(db)
+	blogService := services.NewBlogService(blogRepo)
+	blogController := controllers.NewBlogController(blogService)
+
+	// Group routes under /api for versioning or future scalability
+	api := router.Group("/api")
+	{
+		blogs := api.Group("/posts")
+		{
+			blogs.POST("/", blogController.CreateBlog)
+			blogs.GET("/", blogController.GetAllBlogs)
+			blogs.PUT("/:id", blogController.UpdateBlog)
+			blogs.DELETE("/:id", blogController.DeleteBlog)
+		}
+	}
+}
