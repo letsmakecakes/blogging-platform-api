cd9b5ee54ed7650ca26dfacf3ab9e2da92bbb472
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-17 20:18:44 +0530

    added method to return an object for blog controller

--- internal/controllers/blog_controller.go
+++ internal/controllers/blog_controller.go
@@ -5,3 +5,7 @@ import "bloggingplatformapi/internal/services"
 type BlogController struct {
 	Service services.BlogService
 }
+
+func NewBlogController(service services.BlogService) *BlogController {
+	return &BlogController{service}
+}
