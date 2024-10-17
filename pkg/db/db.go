1df5cba0308eb9a62e0a0a621d5eb83335b242ba
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-17 21:47:36 +0530

    added postgres driver

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,6 +1,10 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
 
 func InitDB(dataSourceName string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", dataSourceName)
