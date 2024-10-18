b5e7475e5c12013725cfa20ced4fc5740481be36
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-18 21:21:54 +0530

    added neccessary logs

--- internal/controllers/blog_controller.go
+++ internal/controllers/blog_controller.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 type BlogController struct {
@@ -23,17 +24,20 @@ func NewBlogController(service services.BlogService) *BlogController {
 func (c *BlogController) CreateBlog(ctx *gin.Context) {
 	var blog models.Blog
 	if err := ctx.ShouldBindJSON(&blog); err != nil {
+		log.Errorf("error binding json: %v", err)
 		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	// Validate the post
 	if err := utils.ValidateBlog(&blog); err != nil {
+		log.Errorf("error validating blog: %v", err)
 		utils.RespondWithError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	if err := c.Service.CreateBlog(&blog); err != nil {
+		log.Errorf("error creating blog: %v", err)
 		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to create blog")
 		return
 	}
@@ -46,12 +50,14 @@ func (c *BlogController) GetBlog(ctx *gin.Context) {
 	idParam := ctx.Param("id")
 	id, err := strconv.Atoi(idParam)
 	if err != nil {
+		log.Errorf("error converting paramater to number: %v", err)
 		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid post ID")
 		return
 	}
 
 	blog, err := c.Service.GetBlogByID(id)
 	if err != nil {
+		log.Errorf("error getting blog: %v", err)
 		if err == sql.ErrNoRows {
 			utils.RespondWithError(ctx, http.StatusNotFound, "Blog not found")
 		} else {
@@ -68,6 +74,7 @@ func (c *BlogController) GetAllBlogs(ctx *gin.Context) {
 	term := ctx.Query("term")
 	blogs, err := c.Service.GetAllBlogs(term)
 	if err != nil {
+		log.Errorf("error getting blogs: %v", err)
 		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to retrieve blogs")
 		return
 	}
@@ -80,24 +87,28 @@ func (c *BlogController) UpdateBlog(ctx *gin.Context) {
 	idParam := ctx.Param("id")
 	id, err := strconv.Atoi(idParam)
 	if err != nil {
+		log.Errorf("error updating blog: %v", err)
 		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid post ID")
 		return
 	}
 
 	var blog models.Blog
 	if err := ctx.ShouldBindJSON(&blog); err != nil {
+		log.Errorf("error binding JSON: %v", err)
 		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	// Validate the blog
 	if err := utils.ValidateBlog(&blog); err != nil {
+		log.Errorf("error validating blog: %v", err)
 		utils.RespondWithError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	blog.ID = id
 	if err := c.Service.UpdateBlog(&blog); err != nil {
+		log.Errorf("error updating blogs: %v", err)
 		if err == sql.ErrNoRows {
 			utils.RespondWithError(ctx, http.StatusNotFound, "Blog not found")
 		} else {
@@ -108,6 +119,7 @@ func (c *BlogController) UpdateBlog(ctx *gin.Context) {
 
 	updatedBlog, err := c.Service.GetBlogByID(id)
 	if err != nil {
+		log.Errorf("error getting blog: %v", err)
 		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to retrieve updated post")
 		return
 	}
@@ -120,11 +132,13 @@ func (c *BlogController) DeleteBlog(ctx *gin.Context) {
 	idParam := ctx.Param("id")
 	id, err := strconv.Atoi(idParam)
 	if err != nil {
+		log.Errorf("error converting parameter to integer: %v", err)
 		utils.RespondWithError(ctx, http.StatusBadRequest, "Invalid blog ID")
 		return
 	}
 
 	if err := c.Service.DeleteBlog(id); err != nil {
+		log.Errorf("error deleting blog: %v", err)
 		if err == sql.ErrNoRows {
 			utils.RespondWithError(ctx, http.StatusNotFound, "Blog not found")
 		} else {
