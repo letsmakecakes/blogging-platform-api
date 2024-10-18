6a6c547a56104931704181c020981ce9905e3979
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-18 20:54:15 +0530

    added comments

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -18,12 +18,13 @@ func SetupRoutes(router *gin.Engine, db *sql.DB) {
 	// Group routes under /api for versioning or future scalability
 	api := router.Group("/api")
 	{
-		blogs := api.Group("/blogs")
+		posts := api.Group("/blogs") // Renamed to 'posts'
 		{
-			blogs.POST("/", blogController.CreateBlog)
-			blogs.GET("/", blogController.GetAllBlogs)
-			blogs.PUT("/:id", blogController.UpdateBlog)
-			blogs.DELETE("/:id", blogController.DeleteBlog)
+			posts.POST("/", blogController.CreateBlog)      // Create a new post
+			posts.GET("/", blogController.GetAllBlogs)      // Get all posts
+			posts.GET("/:id", blogController.GetBlog)       // Get a post by ID
+			posts.PUT("/:id", blogController.UpdateBlog)    // Update a post by ID
+			posts.DELETE("/:id", blogController.DeleteBlog) // Delete a post by ID
 		}
 	}
 }
