44c4eac9f814012673f17aece63a17f653e8b41e
Adwaith Rajeev <adwaithrajeev@gmail.com>
2024-12-25 12:47:43 +0530

    Added necessary comments and error messages.

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,19 +2,21 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
+// InitDB initializes and verifies a connection to a PostgresSQL database.
 func InitDB(dataSourceName string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
 	}
 
 	// Verify connection
 	if err := db.Ping(); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to establish database connection: %w", err)
 	}
 
 	return db, nil
